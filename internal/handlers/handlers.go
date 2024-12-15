6d60be599369e737f933c3d3261c60d201840eeb
hyperspace-worker <48484710+PWMaestro@users.noreply.github.com>
2024-12-15 23:35:42 +0300

    Add email validation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -83,8 +83,9 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 	form := forms.New(r.PostForm)
 
-	form.Required("first_name", "last_name", "email", "phone")
+	form.Required("first_name", "last_name", "email")
 	form.MinLength("first_name", 3, r)
+	form.IsEmail("email")
 
 	if !form.Valid() {
 		data := make(map[string]interface{})
